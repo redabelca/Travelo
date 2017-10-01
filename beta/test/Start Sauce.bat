694d62c08d728ebc8872ab8093f565c50f087a79
redabelca <redabelca96@gmail.com>
2017-10-01 14:38:32 +0000

    updated test folder

--- beta/test/Start Sauce.bat
+++ beta/test/Start Sauce.bat
@@ -1,2 +1,1 @@
 node sauce.js
-node sauce.js
--- beta/test/sauce.js
+++ beta/test/sauce.js
@@ -1,59 +1,126 @@
-var webdriver = require('selenium-webdriver'),
-  username = "brikawgaro",
-  accessKey = "9bd0341b-4d98-4b52-8ced-6bb26fe67243",
-  args = {
-    'browserName': '',
-    'platform': '',
-    'version': '',
-    'username': username,
-    'accessKey': accessKey
+var l = console.log,
+  r = require,
+  wd = r('selenium-webdriver'),
+  fs = r('fs'),
+  mkdirp = r('mkdirp'),
+  getDirName = r('path').dirname,
+  username = "mklkh",
+  accessKey = "5f287d5c-3d84-4cd3-951b-cd8a41bc51a2",
+  By = wd.By,
+  until = wd.until,
+  prefs = new wd.logging.Preferences().setLevel(wd.logging.Type.BROWSER, wd.logging.Level.SEVERE),
+  combination = {
+    'Windows 10': {
+      'MicrosoftEdge': ['14.14393'],
+      'internet explorer': ['11.0'],
+      'chrome': ['59.0', '60.0'],
+      'firefox': ['54.0'],
+    },
+    'Windows 8.1': {
+      'internet explorer': ['11.0'],
+      'firefox': ['54.0'],
+    },
+    'Windows 8': {
+      'firefox': ['54.0'],
+    },
+    'Windows 7': {
+      'opera': ['12.12']
+    },
+    'Windows XP': {
+      'chrome': ['49.0'],
+      'firefox': ['54.0'],
+      'opera': ['12.12']
+    },
+    'Linux': {
+      'chrome': ['48.0'],
+      'firefox': ['54.0'],
+      'opera': ['12.15']
+    },
+    'OS X 10.8': {
+      'chrome': ['49.0'],
+      'firefox': ['54.0']
+    },
+    'macOS 10.12': {
+      'firefox': ['54.0'],
+      'safari': ['10.0']
+    }
   },
-  browsers=['iPhone','iPad','android','chrome','firefox', 'internet explorer','MicrosoftEdge','opera', 'safari'],
-  OSs = [ 'iOS 10.2' , 'iOS 10' , 'iOS 9.3' , 'iOS 9.2' , 'iOS 9.1' , 'iOS 9.0' , 'iOS 8.4' , 'iOS 8.3' , 'iOS 8.2' , 'iOS 8.1' , 'Android 6.0' , 'Android 5.1' , 'Android 5.0' , 'Android 4.4' , 'Windows 10' , 'Windows 8.1' , 'Windows 8' , 'Windows 7' , 'Windows XP' , 'MacOS Sierra 10.12' , 'MacOS El Capitan 10.11' , 'MacOS Yosemite 10.10' , 'MacOS Mavericks 10.9' , 'MacOS Mountain Lion 10.8' , 'Linux'],
-  browserVersions={
-    chrome:[26,58,'beta','dev'],
-    firefox:[4,53,'beta','dev'],//21.0b1 and 25.0b2 for some OSs
-    ie:[6,11],
-    edge:[13.10586,14.14393],
-    safari:[5.1,10],
-    opera:[11.64,12.12,12.15]
-  },
-  driver;
+  i, current = [];
 
-function test(b, p, v) {
-  args.browserName=b;
-  args.platform=p;
-  args.version=v;
-  driver = new webdriver.Builder().
-  withCapabilities(args).
-  usingServer("http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub").
-  build();
-  driver.get("http://www.goldenhealth.ga");
-  driver.getTitle().then(function(title) {
-    console.log('Page title is: ' + title);
+function writeFileSync(path, contents, typeOfData, cb) {
+  mkdirp(getDirName(path), function (err) {
+    if (err) return cb(err);
+    fs.writeFileSync(path, contents, typeOfData, cb);
+  });
+}
+
+function loop(limit, stepTime, fn, cb) {
+  var i = -1,
+    inter = setInterval(function () {
+      i++;
+      if (i >= limit) {
+        clearInterval(inter);
+        cb(i);
+      } else {
+        fn(i);
+      }
+    }, stepTime);
+}
+for (var platform in combination) {
+  for (var br in combination[platform]) {
+    for (i = 0; i < combination[platform][br].length; i++) {
+      current.push({
+        b: br,
+        p: platform,
+        v: combination[platform][br][i]
+      });
+    }
+  }
+}
+
+function test(iOftest) {
+  !current[iOftest] && l('----------------END------------------');
+  var args = {
+      browserName: current[iOftest].b,
+      platform: current[iOftest].p,
+      version: current[iOftest].v
+    },
+    driver = new wd.Builder().withCapabilities(args).
+  setLoggingPrefs(prefs).
+  usingServer("http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub").build();
+
+  //LOG ERRORS
+  driver.get("https://redabelca.github.io/First/")
+    .then(() => driver.manage().logs().get(wd.logging.Type.BROWSER), reason => {})
+    .then(logs => {
+      fs.writeFileSync('errors.json', logs);
+    }, reason => {});
+
+  driver.executeScript("var div=document.createElement('div');div.className='for-test';div.innerHTML=Math.ceil(document.body.scrollHeight/window.innerHeight);document.body.appendChild(div);");
+
+  //IMGs and VIDEO
+  var numberOfScrolling;
+  driver.findElement(By.className('for-test')).getText().then(txt => {
+    numberOfScrolling = Number(txt);
   });
   
-  driver.wait(function(){
-    return driver.
-    findElement(webdriver.By.id('close-overlay-container')).
-    getText().
-    then(function(txt){
-      return txt=='Skip ad »';
+  for(i=0;i< numberOfScrolling ;i++){
+    driver.takeScreenshot().then((data) => {
+      writeFileSync('OS/' + args.platform + '/' + args.browserName + '/v' + args.version + '/' + i + '.png', data, 'base64');
     })
-  },10000).
-  then(function(){
-    driver.findElement(webdriver.By.id('close-overlay-container')).
-    getText().
-    then(function(txt){
-      console.log(txt);
-      driver.findElement(webdriver.By.id('close-overlay-container')).click();
-    })
+      .then(() => {
+      driver.executeScript("window.scrollBy(0,window.innerHeight-5)");
+    });
+  }
+
+  driver.session_.then(data => {
+    l(data.id_);
   });
-  
-  
-  //Skip ad »
   driver.quit();
-  
-};
+}
 
+test(3);
 
+/*
+loop(current.length,1000,(i)=>{wd.promise.createFlow(()=> {test(i);l('Test number : '+i);} );});
+*/
